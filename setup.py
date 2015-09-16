e847149090ef82c4e3274004ad19387c4e65c7fc
MikeCBI <mike@consbio.org>
2015-09-16 14:42:13 -0700

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from os import path
+
+setup(
+    name='EEMS',
+    version='2.0',
+    description='Environmental Evaluation Modeling System',
+    url='https://github.com/consbio/EEMS',
+    author='Tim Sheehan, Ph.D',
+    author_email='Tim@consbio.org',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Climate Modeling :: Tools',
+        'Programming Language :: Python :: 2.7'
+    ],
+    keywords='climate modeling eems',
+    packages=find_packages()
+)
