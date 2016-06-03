168008f60a6ee3553f9c9a43fe6bf5e9d4046550
MikeTheReader <mlundin617@gmail.com>
2016-06-03 10:48:53 -0700

    Modify .gitignore

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from setuptools import setup, find_packages
-from os import path
+from setuptools import setup
 
 setup(
     name='EEMS',
@@ -16,5 +15,6 @@ setup(
         'Programming Language :: Python :: 2.7'
     ],
     keywords='environment,modeling,eems',
-    packages=['']
+    packages=['eems'],
+    install_requires=['numpy'],
 )
